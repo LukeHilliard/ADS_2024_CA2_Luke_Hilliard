8325c88339b1543253891bdf3471398b16e016fc
Luke Hilliard <lukehilliard2001@gmail.com>
2024-11-25 11:29:37 +0000

    3rd time commiting this..... I dont like VS Code
    The first commit, when I pulled for the first time the Entity
    class just disappeared. The second time The Entity class was there
    but no code was present. This time im commiting straight to main.

--- ADS_2024_CA2_Luke_Hilliard/ADS_2024_CA2_Luke_Hilliard/Entity.h
+++ ADS_2024_CA2_Luke_Hilliard/ADS_2024_CA2_Luke_Hilliard/Entity.h
@@ -1,1 +1,44 @@
 #pragma once
+template <typename K, typename V>
+class Entity {
+    K key;
+    V value;
+
+public:
+    Entity(K key, V value);
+    K& getKey();
+    V& getValue();
+    void setKey(K key);
+    void setValue(V value);
+    ~Entity();
+
+
+    Entity::Entity(K key, V value)
+    {
+        this->key = key;
+        this->value = value;
+    }
+
+  
+    K& Entity::getKey() {
+        return key;
+    }
+
+    V& Entity::getValue() {
+        return value;
+    }
+
+    void Entity::setKey(K key) {
+        this->key = key;
+    }
+
+    void Entity::setValue(V value) {
+        this->value = value;
+    }
+
+    Entity::~Entity()
+    {
+        this->key = nullptr;
+        this->value = nullptr;
+    }
+};
