8ce4fe02bc6afbb705e6d4294b000f0770ccd259
Luke Hilliard <lukehilliard2001@gmail.com>
2024-11-30 23:06:55 +0000

    Tested containsKey()

--- ADS_2024_CA2_Luke_Hilliard/TestTreeMap/TestTreeMap.cpp
+++ ADS_2024_CA2_Luke_Hilliard/TestTreeMap/TestTreeMap.cpp
@@ -14,5 +14,27 @@ namespace TestTreeMap
 			tree.put("First", 2);
 			Assert::AreEqual(1, tree.size());
 		}
+
+		TEST_METHOD(TestClearTree)
+		{
+			TreeMap<std::string, int> tree;
+			tree.put("First", 1);
+			tree.put("Second", 2);
+			tree.put("Third", 3);
+
+			tree.clear();
+			Assert::IsTrue(tree.size() == 0);
+		}
+
+		TEST_METHOD(TestContainsKey)
+		{
+			TreeMap<int, std::string> tree;
+			tree.put(1, "Apple");
+			tree.put(30, "Banana");
+			tree.put(2, "Orange");
+			tree.put(5, "Pear");
+
+			Assert::IsTrue(tree.containsKey(30));
+		}
 	};
 }
